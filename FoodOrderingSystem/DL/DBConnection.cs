553ac9988b025308240e09c0f86fd09e0edd4a20
mrtx99 <tahirabbas.taf@gmail.com>
2022-07-16 07:01:16 -0700

    <commit>

---
+++ FoodOrderingSystem/DL/DBConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrderingSystem.DL
+{
+    class DBConnection
+    {
+        private SqlConnection con;
+        public DBConnection()
+        {
+
+            string Path = Environment.CurrentDirectory;
+            string[] appPath = Path.Split(new string[] { "bin" }, StringSplitOptions.None);
+            AppDomain.CurrentDomain.SetData("DataDirectory", appPath[0]);
+            string conString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + appPath[0] + @"GM.mdf;Integrated Security=True";    
+            con = new SqlConnection(conString);
+        }
+
+        public SqlConnection Con { get => con; }
+    }
+}
---
+++ FoodOrderingSystem/DL/LoginDL.cs
@@ -0,0 +1,63 @@
+﻿using FoodOrderingSystem.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrderingSystem.DL
+{
+    class LoginDL
+    {
+        // This class will open connection with DB and write appropriate queries
+        private DBConnection dbCon;
+        public LoginDL()
+        {
+            dbCon = new DBConnection();
+        }
+        public UserDTO VerifyUserFromDB(LoginDTO lg)
+        {
+            UserDTO retDTO = new UserDTO();
+            try
+            {
+                //1. Apply validations on client and server side both
+                //2. Use parameter passing in queries
+
+
+                dbCon.Con.Open();
+                string queryString = "SELECT * FROM MyUser WHERE UserID=@UserID AND Password=@Password;";
+                //string queryString = "SELECT * FROM MyUser WHERE UserID='" + lg.Username + "' AND Password='" + lg.Password + "';";
+                SqlCommand com = new SqlCommand(queryString, dbCon.Con);
+                com.Parameters.AddWithValue("@UserID", lg.Userid);
+                com.Parameters.AddWithValue("@Password", lg.Password);
+
+                SqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    retDTO.Userid = reader["UserID"].ToString();
+                    retDTO.Username = reader["Name"].ToString();
+                    retDTO.Password = reader["Password"].ToString();
+                    retDTO.Role = reader["Status"].ToString();
+                    retDTO.Phone = reader[""].ToString();
+                    return retDTO;
+
+                }
+            }
+            catch (SqlException ex)
+            {
+                return null;
+
+            }
+            finally
+            {
+                dbCon.Con.Close();
+
+            }
+            return null;
+
+        }
+
+    }
+}
+
